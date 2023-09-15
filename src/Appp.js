f2f687f58726ccb489a4572f882f75184bf08ac0
Pavan Manikanta Penugonda <manipenugonda360@gmail.com>
2023-09-15 21:25:48 +0530

    done

---
+++ src/Appp.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "./Components/Header";
+import Route from "./Components/Route";
+import About from "./Components/About";
+import Home from "./Home";
+import Contact from "./Components/Contact";
+import web from './Images/weblogo.png';
+
+ const Appp= () => {
+  return (
+    <article id="body">
+        <div>
+        <section  id="head">
+        <img src={web} alt='Myimage'style={{width:"15%",height:"100px",borderRadius:"50%"}}/>
+     <nav  className="navbar">
+      <Header />
+      </nav>
+      </section>
+      </div>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/About">
+        <About />
+      </Route>
+      <Route path="/Contact">
+        <Contact />
+      </Route>
+    </article>
+  );
+};
+export default Appp;
